0fd855d8a91400dcfa402d1495718aa169193b56
Subham Mondal <subhamoffi@gmail.com>
2026-01-12 19:08:17 +0530

    Assing values in the middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import { verify } from "jsonwebtoken";
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, response, Response } from "express";
 dotenv.config();
+
+interface Req extends Request {
+  user: {
+    userId: number;
+    username: string;
+  };
+}
 
 const SECRET = process.env.JWT_SECRET || "";
 
-export function userValid(req: Request, res: Response, next: NextFunction) {
+export function userValid(req: Req, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
 
   if (!authorization)
@@ -19,7 +26,12 @@ export function userValid(req: Request, res: Response, next: NextFunction) {
       .status(401)
       .json({ success: false, error: "Token missing after Bearer" });
   try {
-    const user = verify(token, SECRET);
+    const user = verify(token, SECRET) as { userId: number; username: string };
+    req.user = {
+      userId: user.userId,
+      username: user.username,
+    };
+    next();
   } catch (error) {
     console.log(error);
     return res.status(401).json({ success: false, error: "Token invalid" });
